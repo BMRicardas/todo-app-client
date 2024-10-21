6454faefac57aa27dc1bb826fdeeeb9e55ebf7b3
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 21:31:07 +0300

    remove configuration

--- config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  API_URL:
-    process.env.NODE_ENV === "production"
-      ? "todo-app-server-alpha.vercel.app"
-      : "http://localhost:8000",
-};
-
-console.log("Config", { config });
