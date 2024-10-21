79ce926559cb89799af84c5096951cece37d9b00
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 19:20:54 +0300

    refactor text input component styles and structure

--- src/components/common/text-input/text-input.component.tsx
+++ src/components/common/text-input/text-input.component.tsx
@@ -1,8 +1,9 @@
+import React, { useId } from "react";
 import { RegisterOptions, useFormContext } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
+import { clsx } from "clsx";
 
 import styles from "./text-input.module.css";
-import { useId } from "react";
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
@@ -10,15 +11,23 @@ type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   options?: RegisterOptions;
 };
 
-export function TextInput({ label, name, options, ...props }: Props) {
+export function TextInput({
+  label,
+  name,
+  options,
+  className,
+  ...props
+}: Props) {
   const id = useId();
   const {
     register,
     formState: { errors },
   } = useFormContext();
 
+  const hasError = !!errors[name];
+
   return (
-    <div className={styles.inputGroup}>
+    <div className={clsx(styles.inputGroup, className)}>
       {label && (
         <label className={styles.label} htmlFor={id}>
           {label}
@@ -28,13 +37,15 @@ export function TextInput({ label, name, options, ...props }: Props) {
         <input
           type="text"
           id={id}
-          className={styles.input}
+          className={clsx(styles.input, {
+            [styles.error]: hasError,
+          })}
           {...register(name, options)}
           {...props}
         />
         <ErrorMessage
           errors={errors}
-          name="text"
+          name={name}
           render={({ message }) => (
             <p className={styles.errorMessage}>{message}</p>
           )}
