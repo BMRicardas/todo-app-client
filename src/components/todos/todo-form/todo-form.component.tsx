0a3391250ecdbbd8aae270faef508dca083e336e
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 19:21:02 +0300

    refactor todo-form.component.tsx and todo-form.module.css

--- src/components/todos/todo-form/todo-form.component.tsx
+++ src/components/todos/todo-form/todo-form.component.tsx
@@ -2,9 +2,10 @@ import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
+import { useTodoStore } from "@/stores/todo.store";
+
 import { TextInput } from "@/components/common/text-input/text-input.component";
 import { Button } from "@/components/common/button/button.component";
-import { Loading } from "@/components/common/loading/loading.coponent";
 
 import styles from "./todo-form.module.css";
 
@@ -17,51 +18,23 @@ const todoSchema = z.object({
 
 type TodoFormValues = z.infer<typeof todoSchema>;
 
-interface Props {
-  onSubmit: (data: TodoFormValues) => Promise<void>;
-  isSubmitting?: boolean;
-}
-
-export function TodoForm({ onSubmit, isSubmitting }: Props) {
+export function TodoForm() {
+  const { addTodo } = useTodoStore();
   const methods = useForm<TodoFormValues>({
     resolver: zodResolver(todoSchema),
-    defaultValues: {
-      text: "",
-    },
   });
 
-  const { reset, handleSubmit } = methods;
-
-  const onSubmitHandler = async (data: TodoFormValues) => {
-    try {
-      await onSubmit(data);
-      reset();
-    } catch (error) {
-      console.error("Failed to add todo:", error);
-      methods.setError("text", {
-        type: "submit",
-        message: "Failed to add todo. Please try again.",
-      });
-    }
+  const onSubmit = async (data: TodoFormValues) => {
+    await addTodo(data.text);
+    methods.reset();
   };
 
   return (
     <FormProvider {...methods}>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmitHandler)}>
-        <TextInput
-          name="text"
-          placeholder="What needs to be done?"
-          disabled={isSubmitting}
-        />
-        <Button type="submit" disabled={isSubmitting} variant="submit">
-          {isSubmitting ? (
-            <>
-              <Loading size="sm" />
-              <span>Adding...</span>
-            </>
-          ) : (
-            "Add Todo"
-          )}
+      <form className={styles.form} onSubmit={methods.handleSubmit(onSubmit)}>
+        <TextInput name="text" placeholder="What needs to be done?" />
+        <Button type="submit" color="primary">
+          Add Todo
         </Button>
       </form>
     </FormProvider>
