2104a9ea542cb25f179b9c1b248457e68a723dfa
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 20:48:25 +0300

    refactor API_URL configuration to use alpha server in production

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const config = {
   API_URL:
     process.env.NODE_ENV === "production"
-      ? "todo-app-server-eta.vercel.app"
+      ? "todo-app-server-alpha.vercel.app"
       : "http://localhost:8000",
 };
 
