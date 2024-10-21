dbc90bd129eb2dfa7bf2d0c4932a17800d0d7ece
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 19:21:07 +0300

    refactor todo-item component to use Zustand for managing todos

--- src/components/todos/todo-item/todo-item.component.tsx
+++ src/components/todos/todo-item/todo-item.component.tsx
@@ -1,159 +1,131 @@
-// todo-item.component.tsx
 import { useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-
+import styles from "./todo-item.module.css";
 import { Todo } from "@/types/todo";
+
+import { FormProvider, useForm } from "react-hook-form";
 import { CheckboxInput } from "@/components/common/checkbox-input/checkbox-input.component";
 import { TextInput } from "@/components/common/text-input/text-input.component";
 import { Button } from "@/components/common/button/button.component";
-import { Loading } from "@/components/common/loading/loading.coponent";
 
-import styles from "./todo-item.module.css";
+import { clsx } from "clsx";
+import { useTodoStore } from "@/stores/todo.store";
 
 type Props = {
   todo: Todo;
-  onStatusChange: (id: number, status: boolean) => Promise<void>;
-  onDelete: (id: number) => Promise<void>;
-  onEdit: (id: number, todo: Todo) => Promise<void>;
 };
 
-export function TodoItem({ todo, onStatusChange, onDelete, onEdit }: Props) {
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [isUpdating, setIsUpdating] = useState(false);
+export const TodoItem = ({ todo }: Props) => {
   const [isEditing, setIsEditing] = useState(false);
-
-  const methods = useForm({
-    defaultValues: {
-      text: todo.text,
-      status: todo.status,
-    },
+  const { editTodo, deleteTodo } = useTodoStore();
+  const methods = useForm<Todo>({
+    defaultValues: todo,
   });
 
-  const { setError, reset, handleSubmit, watch } = methods;
-
-  const text = watch("text");
+  console.log(`TodoItem rendering for todo: ${todo._id}`);
 
   const handleStatusChange = async () => {
-    if (isUpdating) return;
-
     try {
-      setIsUpdating(true);
-      await onStatusChange(todo._id, !todo.status);
+      await editTodo(todo._id, { ...todo, status: !todo.status });
     } catch (error) {
       console.error("Failed to update todo status:", error);
+    }
+  };
+
+  const handleEdit = async (editedTodo: Todo) => {
+    if (editedTodo.text === todo.text) {
+      toggleEditMode();
+      return;
+    }
+
+    console.log("data:", editedTodo);
+    try {
+      await editTodo(todo._id, editedTodo);
+    } catch (error) {
+      console.error("Failed to edit todo:", error);
     } finally {
-      setIsUpdating(false);
+      toggleEditMode();
     }
   };
 
   const handleDelete = async () => {
-    if (isDeleting) return;
-
     try {
-      setIsDeleting(true);
-      await onDelete(todo._id);
+      await deleteTodo(todo._id);
     } catch (error) {
       console.error("Failed to delete todo:", error);
-    } finally {
-      setIsDeleting(false);
     }
   };
 
-  const handleEdit = async (data: { text: string; status: boolean }) => {
-    try {
-      await onEdit(todo._id, data);
-      setIsEditing(false);
-    } catch (error) {
-      console.error("Failed to edit todo:", error);
-      setError("text", {
-        type: "submit",
-        message: "Failed to edit todo. Please try again.",
-      });
-    }
-  };
-
-  function handleCancelEdit() {
-    reset({ text: todo.text });
-    setIsEditing(false);
+  function toggleEditMode() {
+    setIsEditing((prev: boolean) => !prev);
   }
 
   return (
     <div className={styles.todoItem}>
       <FormProvider {...methods}>
-        <form
-          className={styles.todoContent}
-          onSubmit={handleSubmit(handleEdit)}>
-          <div className={styles.mainContent}>
+        <form onSubmit={methods.handleSubmit(handleEdit)}>
+          <div className={styles.todoContent}>
             <CheckboxInput
               name="status"
               checked={todo.status}
               onChange={handleStatusChange}
-              disabled={isUpdating || isEditing}
             />
 
             {isEditing ? (
-              <div className={styles.editInput}>
-                <TextInput
-                  name="text"
-                  options={{
-                    required: "Todo text is required",
-                    minLength: {
-                      value: 3,
-                      message: "Todo must be at least 3 characters",
-                    },
-                  }}
-                />
-              </div>
+              <TextInput
+                name="text"
+                autoFocus
+                options={{
+                  required: "Todo text is required",
+                  minLength: {
+                    value: 3,
+                    message: "Todo must be at least 3 characters",
+                  },
+                }}
+              />
             ) : (
               <span
-                className={`${styles.todoText} ${
-                  todo.status ? styles.completed : ""
-                }`}>
+                className={clsx(styles.todoText, {
+                  [styles.completed]: todo.status,
+                })}>
                 {todo.text}
               </span>
             )}
-          </div>
-          <div className={styles.actions}>
-            {isEditing ? (
-              todo.text === text ? (
-                <Button type="button" onClick={handleCancelEdit} variant="edit">
-                  Cancel
-                </Button>
+            <div className={styles.actions}>
+              {isEditing ? (
+                <>
+                  <Button type="submit" variant="success">
+                    Save
+                  </Button>
+                  <Button
+                    type="button"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      toggleEditMode();
+                    }}
+                    variant="secondary">
+                    Cancel
+                  </Button>
+                </>
               ) : (
-                <Button
-                  type="submit"
-                  variant="edit"
-                  disabled={methods.formState.isSubmitting}>
-                  Save
-                </Button>
-              )
-            ) : (
-              <Button
-                type="button"
-                onClick={() => setIsEditing(true)}
-                variant="edit">
-                Edit
-              </Button>
-            )}
-            {!isEditing ? (
-              <Button
-                type="button"
-                onClick={handleDelete}
-                variant="delete"
-                disabled={isDeleting || isEditing}>
-                {isDeleting ? (
-                  <>
-                    <Loading size="sm" />
-                    <span>Deleting...</span>
-                  </>
-                ) : (
-                  "Delete"
-                )}
-              </Button>
-            ) : null}
+                <>
+                  <Button
+                    type="button"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      toggleEditMode();
+                    }}
+                    variant="primary">
+                    Edit
+                  </Button>
+                  <Button type="button" onClick={handleDelete} variant="danger">
+                    Delete
+                  </Button>
+                </>
+              )}
+            </div>
           </div>
         </form>
       </FormProvider>
     </div>
   );
-}
+};
