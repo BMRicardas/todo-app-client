658d7d58468e5e57b782b8a2561d5ac6534484fd
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-21 20:25:29 +0300

    refactor API_URL configuration to use VERCEL_ENV variable

--- config.ts
+++ config.ts
@@ -1,7 +1,6 @@
 export const config = {
-  // Use environment variable in production, localhost in development
   API_URL:
-    process.env.NODE_ENV === "production"
+    process.env.VERCEL_ENV === "production"
       ? "https://todo-app-server-3jo80m8xf-bmricardas-projects.vercel.app"
       : "http://localhost:8000",
 };
